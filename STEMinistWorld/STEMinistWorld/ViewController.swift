1f59ad1f9af829807d1810dbb9436c075d46f97e
Kavya Tewari <kavya.tewari@gmail.com>
2015-07-13 09:21:47 -0700

    Initial Commit

---
+++ STEMinistWorld/STEMinistWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  STEMinistWorld
+//
+//  Created by Kavya on 7/13/15.
+//  Copyright (c) 2015 Kavya Tewari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
