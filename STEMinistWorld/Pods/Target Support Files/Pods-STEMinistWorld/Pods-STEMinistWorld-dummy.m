388c2379e0bc852d2b5b1bb2a4fb60ffabbeaddd
Kavya Tewari <kavya.tewari@gmail.com>
2015-08-03 15:41:25 -0700

    pod

---
+++ STEMinistWorld/Pods/Target Support Files/Pods-STEMinistWorld/Pods-STEMinistWorld-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_STEMinistWorld : NSObject
+@end
+@implementation PodsDummy_Pods_STEMinistWorld
+@end
---
+++ STEMinistWorld/Pods/Target Support Files/Pods-STEMinistWorldTests/Pods-STEMinistWorldTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_STEMinistWorldTests : NSObject
+@end
+@implementation PodsDummy_Pods_STEMinistWorldTests
+@end
